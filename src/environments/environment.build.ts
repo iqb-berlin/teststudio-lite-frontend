c76fd69461e4eaf4bceb04c8b5e6a9f739b7b2d9
rhenck <richard.henck@iqb.hu-berlin.de>
2021-03-16 11:59:20 +0100

    Clean up environment files
    Dev variant is now default and prod points to 'api' as backend path. 
    This is how we do it for the testcenter.

--- src/environments/environment.build.ts
+++
@@ -1,9 +0,0 @@
-// ng build --configuration=build
-
-export const environment = {
-  production: false,
-  backendUrl: '/',
-  appName: 'IQB-ItemDB',
-  appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  appVersion: '0.8.4 - 11.3.2019'
-};
